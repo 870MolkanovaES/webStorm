b3f31e437894a578d821019018e41be3a5af68a5
Kate Molk <870molkanovaes@gmail.com>
2020-10-16 19:24:35 +0300

    16.10.2020

--- template1-app/src/components/Blog.jsx
+++ template1-app/src/components/Blog.jsx
@@ -3,48 +3,64 @@ import {NavLink} from "react-router-dom";
 
 const BlogItem=(props)=>{
     return <div className="blog-post-item">
-        <img src={props.img} alt="" className="blog-thumb"/>
+        <img src={"img/"+props.img} alt="" className="blog-thumb"/>
         <div className="post-content">
             <div className="post-cata">{props.category}</div>
             <h4 className="post-title">{props.title}</h4>
             <div className="post-date">{props.date}</div>
             <p>{props.description}</p>
-            <NavLink to="#" className="site-btn sb-big">Читать дальше <img
+            <NavLink to={"blog/"+props.category.replaceAll(" ","_")} className="site-btn sb-big">Читать дальше <img
                 src="img/icons/arrow-right-black.png" alt=""/></NavLink>
         </div>
     </div>
 }
 
-const Blog = (props)=>{
-    let articles=props.getArticles();
-    let articlesCount=Object.keys(articles).length;
-    let blogItems=[];
-    for (let i=0;i<articlesCount;i++){
-        blogItems.push(<BlogItem
-            key={i}
-            title={articles[i].category}
-            title1={articles[i].title}
-            date={articles[i].date}
-            description={articles[i].description}
-            img={articles[i].img}
-        />)
+class Blog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {blogItems: []};
     }
-    return <div>
+    componentDidMount() {
+        this.props.getArticles().then(
+            articles=>{
+                let articlesCount = articles.length;
+                let blogItems = [];
+                for (let i = 0; i < articlesCount; i++) {
+                    blogItems.push(
+                        <BlogItem
+                            key={i}
+                            category={articles[i].category}
+                            title={articles[i].title}
+                            date={articles[i].date}
+                            description={articles[i].description}
+                            img={articles[i].img}
+                        />)
+                }
+                this.setState({blogItems: blogItems})
+            }
+        )
+
+    }
+
+    render() {
+
+            return <div>
                 <section className="blog-page page-warp">
                     <div className="sp-container">
                         <div className="page-title">
                             <h2>Новости</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis et augue non mollis. Sed
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis et augue non mollis.
+                                Sed
                                 sagittis.</p>
                         </div>
                         <div className="row m-0">
                             <div className="col-lg-4 col-sm-6 p-0 artists-col">
-                                {blogItems}
+                                {this.state.blogItems}
                             </div>
                         </div>
                     </div>
                 </section>
-    </div>
+            </div>
+    }
 }
-
 export default Blog;
